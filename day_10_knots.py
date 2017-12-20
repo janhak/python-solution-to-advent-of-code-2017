634416741dc70306c7b586e841b260f5b08b8dd1
Jan Chwiejczak <jchwiejczak@gmail.com>
2017-12-20 00:27:42 +0000

    Remove comment

--- day_10_knots.py
+++ day_10_knots.py
@@ -153,7 +153,6 @@ class TestKnotHash(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    # get input
     raw_input = '212,254,178,237,2,0,1,54,167,92,117,125,255,61,159,164'
     input_list = input_from_bytes(raw_input)
     knot = KnotHash(256, input_list)
